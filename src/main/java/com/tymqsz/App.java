a1f3d831e8d65fa9f0fc3069df7b80af117a2e43
Tymon Piwowarski <topiwowarski@gmail.com>
2024-07-15 13:21:46 +0200

    fix imdb.txt

--- src/main/java/com/tymqsz/App.java
+++ src/main/java/com/tymqsz/App.java
@@ -1,8 +1,7 @@
 package com.tymqsz;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
+
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -50,29 +49,6 @@ public class App
             }   
         }
         
-        List<MovieRecord> mcList = database.getMcList();
-        Collections.sort(mcList);
-        
-        int wrongCnt = 0;
-        for(int i = 0; i < 250; i++){
-            int nullCnt = 0;
-            String str = mcList.get(i).toString();
-            for(int j = 0; j < str.length()-4; j++){
-                String sub = str.substring(j, j+4);
-
-                if(sub.equals("null")){
-                    nullCnt++;
-                }
-            }
-
-            if(nullCnt == 11){
-                wrongCnt++;
-                System.out.println(str.substring(0, 20));
-            }
-
-        }
-
-        System.out.println(wrongCnt);
         driver.quit();
     }
 
